cfcec117f51ed22f63b3b477d515c314a4c3d587
Mario Martínez <mario@foo.cl>
2017-01-04 23:27:47 -0300

    agregado filtro por proyecto

--- rpt.py
+++ rpt.py
@@ -4,7 +4,7 @@ import tareas
 import datetime
 import time
 
-def histograma(dias=None,desde=None,hasta=None,archivo='reporte.pdf'):
+def histograma(dias=None,desde=None,hasta=None,archivo='reporte.pdf',proyectos=None):
     """ histograma de hh en periodo por dia """
     if hasta is None:
         hasta = datetime.datetime.today().date()
@@ -19,7 +19,7 @@ def histograma(dias=None,desde=None,hasta=None,archivo='reporte.pdf'):
     horas   = []
     for x in range(0,dias):
         fechas.append(hasta - datetime.timedelta(days=x))
-        horas.append(hh((hasta - datetime.timedelta(days=x))))
+        horas.append(hh((hasta - datetime.timedelta(days=x)),proyectos=proyectos))
     plt.gcf().subplots_adjust(bottom=0.25)
     plt.title(u'HH por dia')
     plt.xlabel(u'Dias')
@@ -28,11 +28,11 @@ def histograma(dias=None,desde=None,hasta=None,archivo='reporte.pdf'):
     plt.xticks(fechas, fechas, rotation=70)
     plt.savefig(archivo)
 
-def hh(desde,delta=1):
+def hh(desde,delta=1,proyectos=None):
     total = 0
     de = time.mktime(desde.timetuple())
     ha = time.mktime((desde+datetime.timedelta(delta)).timetuple())
-    for t in tareas.tareas(imprimir=False,desde=de,hasta=ha):
+    for t in tareas.tareas(imprimir=False,desde=de,hasta=ha,proyectos=proyectos):
         total = t.hh(desde=de,hasta=ha) + total
     return total
 
@@ -41,11 +41,12 @@ def main():
     a.add_argument('-i','--histograma',action='store_true',help='histograma de tareas')
     a.add_argument('-d','--dias',type=int)
     a.add_argument('-a','--archivo')
+    a.add_argument('-fp','--filtroproyecto',type=int,action='append',help='filtro proyecto')
     a.add_argument('-de','--desde',help='desde')
     a.add_argument('-ha','--hasta',help='hasta')
     p = a.parse_args()
     if p.histograma:
-        histograma(desde=p.desde,hasta=p.hasta,dias=p.dias,archivo=p.archivo)
+        histograma(desde=p.desde,hasta=p.hasta,dias=p.dias,archivo=p.archivo,proyectos=p.filtroproyecto)
 
 if __name__ == '__main__':
     main()
